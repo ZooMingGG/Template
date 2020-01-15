bf42b8b44f7aea70f706489ecd3a5cdeb581066f
ZooMingGG <volodia.lukashewwich@gmail.com>
2020-01-15 18:15:09 +0200

    add a js part of burger menu

---
+++ js/index.js
@@ -0,0 +1,7 @@
+'use strict'
+function showNav() {
+    let toggle = document.getElementById('toggle');
+    toggle.classList.toggle('toggle-active');
+    let nav = document.getElementById('nav');
+    nav.classList.toggle('nav-active');
+}
